fb03a7b5c586bb2d574e3bf6ff396d3d0e21b029
Joshua Gerald <gerald85joshua@gmail.com>
2023-03-09 23:49:48 +0700

    Initial project

---
+++ SistemPendataanJemaat/SistemPendataanJemaat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemPendataanJemaat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
