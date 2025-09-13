47f5719f759549d4001230786eb9224e30d0520b
Joshua Hutasoit <gerald85joshua@gmail.com>
2025-09-13 17:50:05 +0700

    Auto open browser

--- SistemPendataanJemaat/SistemPendataanJemaat/Program.cs
+++ SistemPendataanJemaat/SistemPendataanJemaat/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace SistemPendataanJemaat
 {
@@ -7,7 +9,17 @@ namespace SistemPendataanJemaat
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            // Start the host
+            host.Start();
+
+            // Open browser to localhost
+            // OpenBrowser("http://localhost:5000"); // http
+            OpenBrowser("https://localhost:5001"); // https
+
+            // Wait for the host to shutdown
+            host.WaitForShutdown();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -16,5 +28,28 @@ namespace SistemPendataanJemaat
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void OpenBrowser(string url)
+        {
+            try
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+            }
+            catch
+            {
+                // Ignore exceptions
+            }
+        }
     }
 }
